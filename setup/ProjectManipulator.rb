968f29e095a76f6ff7517f5918bcf7310270d81f
Ronan Le Boïté <ronan@growabanana.com>
2018-11-10 11:26:00 -0800

    remove remove demo target param

--- setup/ProjectManipulator.rb
+++ setup/ProjectManipulator.rb
@@ -13,7 +13,6 @@ module Pod
       @xcodeproj_path = options.fetch(:xcodeproj_path)
       @configurator = options.fetch(:configurator)
       @platform = options.fetch(:platform)
-      @remove_demo_target = options.fetch(:remove_demo_project)
       @prefix = options.fetch(:prefix)
     end
 
